41b290de87734265fe2219550452f2168d42989c
Dani Paolo S. Dullano <pdullano@gmail.com>
2020-03-02 18:44:02 +0800

    Updates

---
+++ precache-manifest.859bacfca1e3c8182559a10fcd8063e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c55dd0bc6acc22a1d67e99d2c89b00c",
+    "url": "/react-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "4a06dddc8fc393a10754",
+    "url": "/react-pomodoro-clock/static/css/main.5b8ddd71.chunk.css"
+  },
+  {
+    "revision": "efe405cba66874f832ae",
+    "url": "/react-pomodoro-clock/static/js/2.0ff0ebb1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-pomodoro-clock/static/js/2.0ff0ebb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a06dddc8fc393a10754",
+    "url": "/react-pomodoro-clock/static/js/main.7bd4afe8.chunk.js"
+  },
+  {
+    "revision": "608933af8d842245cca4",
+    "url": "/react-pomodoro-clock/static/js/runtime-main.218f3656.js"
+  }
+]);
